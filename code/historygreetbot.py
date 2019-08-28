7ef208d3d2ca754374ef423aab7693f94f606498
John Harney <john.harney@centre.edu>
2019-08-28 15:52:58 -0400

    Update historygreetbot.py
    THIS WORKS!!! Tested 08-28-19 at 3:47pm and it WORKS!

--- code/historygreetbot.py
+++ code/historygreetbot.py
@@ -1,25 +1,32 @@
-# First attempt to unify code, I want the PIR to detect motion and then the RPi to generate an event response
-# Code here is heavily dependent on work of seesunmoon
+# this code exists thanks to Texas-Mark on the official RPi forums, he shared this at https://www.raspberrypi.org/forums/viewtopic.php?t=176241
 
 import RPi.GPIO as GPIO
-import subprocess
-from datetime import datetime
 import time
-
 import os
-file = "file.mp3"
 
-movie_length = 10 # The length of the movie in seconds
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
-GPIO.setmode(GPIO.BOARD)
-pir = 7 # Change to your connected GPIO pin
-GPIO.setup(pir, GPIO.IN)
+pir_sensor = 4 #GPIO pin 4
+led = 21 #GPIO pin 21
+myCmd = 'omxplayer -o alsa lincoln.mp3'
 
-print "Sensor initialized at " + str(datetime.now())
-time.sleep(3)
+GPIO.setup(pir_sensor, GPIO.IN, GPIO.PUD_DOWN)
 
-print "Detecting motion at " + str(datetime.now())
+current_state = 0
+GPIO.setup(led,GPIO.OUT)
 
 while True:
-    if GPIO.input(pir):
-        os.system("omxplayer " + file)
+    try:
+        time.sleep(0.1)
+        current_state = GPIO.input(pir_sensor)
+        if current_state == 1:
+          print("Lincoln Bot Activated") # motion detected
+          os.system(myCmd) # plays the mp3
+          GPIO.output(led,True) #Turn on LED
+          time.sleep(2) # leave LED on for 2 seconds
+          GPIO.output(led,False) #turn off LED
+          time.sleep(4) # wait 4 seconds for PIR to reset. 
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        
